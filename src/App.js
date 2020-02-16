0d85c2c957e5554855bf76dc8001a6dfa7ebe4e1
Pietrzi <pietrzi@op.pl>
2020-02-16 20:33:14 +0100

    finished ui components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { getUsersRequest } from './actions/users';
+import React, {Component} from 'react';
+import NewUserForm from './components/NewUserForm';
+import UserList from './components/UserList';
+import {connect} from 'react-redux';
+import {getUsersRequest, createUserRequest, deleteUserRequest, usersError} from '../actions/users';
+import {Alert} from 'reactstrap';
 
+class App extends Component {
+    constructor(props){
+        super(props);
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+        this.props.getUsersRequest();
+    }
 
-    this.props.getUsersRequest();
-  }
+    handleCreateUserSubmit = ({firstName, lastName}) => {
+        this.props.createUserRequest({
+            firstName,
+            lastName
+        });
+    };
 
-  render() {
+    handleDeleteUserClick = (userId) => {
+        this.props.deleteUserRequest(userId);
+    };
 
-    return (
-      <div>
-        test
-      </div>
-    );
-  }
-  
+    handleCloseAlert = () => {
+        this.props.usersError({
+            error: ''
+        });
+    };
+
+    render(){
+        const users = this.props.users;
+        return (
+            <div style={{margin: '0 auto', padding: '20px', maxWidth: '600px'}}>
+                <h2>
+                    Users
+                </h2>
+                <Alert color="danger" isOpen={!!this.props.users.error} toggle={this.handleCloseAlert}>
+                    {this.props.users.error}
+                </Alert>
+                <NewUserForm onSubmit={this.handleCreateUserSubmit} />
+                {!!users.items && !!users.items.length &&
+                <UserList onDeleteUserClick={this.handleDeleteUserClick} users={users.items}/>
+                }
+            </div>
+        );
+    }
 }
 
-export default connect(null, {
-  getUsersRequest
+export default connect(({users}) => ({users}), {
+    getUsersRequest,
+    createUserRequest,
+    deleteUserRequest,
+    usersError
 })(App);
